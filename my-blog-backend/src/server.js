93dd1320eb6cae5e403e4dcb2b945bf0850bfd82
Anand_Pawar <apawar3099@gmail.com>
2021-06-18 04:31:15 +0530

    refractored code by creating withDB() function to reduce re-writing

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -35,34 +35,43 @@ app.use(bodyParser.json())
 // });
 
 
-app.get("/api/articles/:name", async (req, res) => {
+const withDB = async (operations, res)=>{
 
     try {
-    const articleName = req.params.name;
+        const client = await MongoClient.connect("mongodb://localhost:27017", {useNewUrlParser: true});
+    
+        const db = client.db("my-blog");
+    
+        await operations(db);
 
-    const client = await MongoClient.connect("mongodb://localhost:27017", {useNewUrlParser: true});
+        res.status(200).json(articlesInfo); //json() is same as send()
+    
+        client.close();
+        }
+        catch(error){
+            res.status(500).json({message: "Internal server problem occured, error connecting to DB ", error})
+        }
+    
 
-    const db = client.db("my-blog");
+}
 
-    const articlesInfo = await db.collection("articles").findOne({name : articleName});
+app.get("/api/articles/:name", async (req, res) => {
+    withDB(async (db) => {
 
-    res.status(200).json(articlesInfo); //json() is same as send()
+        const articleName = req.params.name;
 
-    client.close();
-    }
-    catch(error){
-        res.status(500).json({message: "Internal server problem occured, error connecting to DB ", error})
-    }
+        const articlesInfo = await db.collection("articles").findOne({name : articleName});
+    
+        res.status(200).json(articlesInfo); //json() is same as send()
 
-})
+    }, res);
+
+});
 
 app.post("/api/articles/:name/upvote", async (req, res) => {
-    try{
+    withDB(async (db) => {
+    
     const articleName = req.params.name;
-
-    const client = await MongoClient.connect("mongodb://localhost:27017", {useNewUrlParser: true});
-
-    const db = client.db("my-blog");
 
     const articlesInfo = await db.collection("articles").findOne({name : articleName});
 
@@ -76,22 +85,59 @@ app.post("/api/articles/:name/upvote", async (req, res) => {
     const updatedArticlesInfo = await db.collection("articles").findOne({name : articleName});
 
     res.status(200).json(updatedArticlesInfo);
-    client.close();
-    }
-    catch(error){
-        res.status(500).json({message: "Internal server problem occured, error connecting to DB ", error})
-    }
+    }, res);
+    
+    
+})  
+
+// app.post("/api/articles/:name/upvote", async (req, res) => {
+//     try{
+//     const articleName = req.params.name;
+
+//     const client = await MongoClient.connect("mongodb://localhost:27017", {useNewUrlParser: true});
+
+//     const db = client.db("my-blog");
+
+//     const articlesInfo = await db.collection("articles").findOne({name : articleName});
+
+//     await db.collection("articles").updateOne({name : articleName}, 
+//         {'$set' : 
+//         {
+//             upvotes: articlesInfo.upvotes+1,
+//         },
+//     });
+
+//     const updatedArticlesInfo = await db.collection("articles").findOne({name : articleName});
+
+//     res.status(200).json(updatedArticlesInfo);
+//     client.close();
+//     }
+//     catch(error){
+//         res.status(500).json({message: "Internal server problem occured, error connecting to DB ", error})
+//     }
 
     
-})
+// })   // THIS IS POST CODE WITHOUT REFRACTORING
 
 app.post("/api/articles/:name/add-comment", (req, res) => {
     const { username, text } = req.body;
     const articleName = req.params.name;
 
-    articlesInfo[articleName].comments.push({username, text});
+    withDB(async (db) =>{
+        const articlesInfo = await db.collection("articles").findOne({name : articleName});
 
-    res.status(200).send(articlesInfo[articleName]);
+        await db.collection("articles").updateOne({name : articleName}, 
+            {'$set' : 
+            {
+                comments: articlesInfo.comments.concat({username, text}),
+            },
+        });
+        
+        const updatedArticlesInfo = await db.collection("articles").findOne({name : articleName});
+
+        res.status(200).json(updatedArticlesInfo) 
+    },res);
+
 })
 
 
@@ -100,7 +146,7 @@ app.post("/api/articles/:name/add-comment", (req, res) => {
 
 //     articlesInfo[articleName].upvotes +=1;
 //     res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
-// })
+// }) // THIS WAS POST METHOD BEFORE USING MONGODB
 
 // app.post("/api/articles/:name/add-comment", (req, res) => {
 //     const { username, text } = req.body;
@@ -109,6 +155,6 @@ app.post("/api/articles/:name/add-comment", (req, res) => {
 //     articlesInfo[articleName].comments.push({username, text});
 
 //     res.status(200).send(articlesInfo[articleName]);
-// })
+// }) // THIS WAS POST METHOD BEFORE USING MONGODB
 
 app.listen(8000, ()=> console.log("listening on PORT 8000 "))
