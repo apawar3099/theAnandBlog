d63945d514a547ac506fc2764495cd74ee8be1af
Anand_Pawar <apawar3099@gmail.com>
2021-06-17 18:40:11 +0530

    updated pages and added their routes

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -4,12 +4,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesList from './pages/ArticlesList';
+
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Route path="/" component={HomePage} exact />
+        <div id="page-body">
+          <Route path="/" component={HomePage} exact />
+          <Route path="/about" component={AboutPage} exact />
+          <Route path="/article" component={ArticlePage} exact />
+          <Route path="/articles-list" component={ArticlesList} exact />
+        </div>
+        
+
       </div>
     </Router>
     
---
+++ my-blog/src/pages/AboutPage.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const AboutPage = ()=> (
+    <>
+    <h1>About Me !!</h1>
+    <p>
+        Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more! You can control how much you get, place it within HTML structure as it expands, and get different bits of it in repeated elements.
+    </p>
+    <p>
+        Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. 
+    </p>
+    <p>
+        Emmet is great for that. With it installed in the code editor you are using, you can type “lorem” and then tab and it will expand into a paragraph of Lorem Ipsum placeholder text. But it can do more! You can control how much you get, place it within HTML structure as it expands, and get different bits of it in repeated elements.
+    </p>
+    </>
+);
+
+export default AboutPage;
