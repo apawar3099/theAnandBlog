0d521104d89900d0e030d935c12fac0430b39a91
Anand_Pawar <apawar3099@gmail.com>
2021-06-17 18:26:35 +0530

    Added react-router through npm

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
+import React from 'react';
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 import HomePage from './pages/HomePage';
 
 function App() {
   return (
-    <div className="App">
-      <HomePage/>
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/" component={HomePage} exact />
+      </div>
+    </Router>
+    
   );
 }
 
