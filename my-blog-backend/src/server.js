7387bd11516bebe84584532d29477c746f9bd49d
Anand_Pawar <apawar3099@gmail.com>
2021-06-18 03:42:52 +0530

    added get feature to fetch data from mongo

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -1,5 +1,8 @@
 import express from "express"
 import bodyParser from "body-parser"
+
+import {MongoClient} from 'mongodb';
+
 const app = express();
 
 app.use(bodyParser.urlencoded())
@@ -32,22 +35,41 @@ app.use(bodyParser.json())
 // });
 
 
-app.
+app.get("/api/articles/:name", async (req, res) => {
 
-app.post("/api/articles/:name/upvote", (req, res) => {
+    try {
     const articleName = req.params.name;
 
-    articlesInfo[articleName].upvotes +=1;
-    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
+    const client = await MongoClient.connect("mongodb://localhost:27017", {useNewUrlParser: true});
+
+    const db = client.db("my-blog");
+
+    const articlesInfo = await db.collection("articles").findOne({name : articleName});
+
+    res.status(200).json(articlesInfo); //json() is same as send()
+
+    client.close();
+    }
+    catch(error){
+        res.status(500).json({message: "Internal server problem occured, error connecting to DB ", error})
+    }
+
 })
 
-app.post("/api/articles/:name/add-comment", (req, res) => {
-    const { username, text } = req.body;
-    const articleName = req.params.name;
+// app.post("/api/articles/:name/upvote", (req, res) => {
+//     const articleName = req.params.name;
 
-    articlesInfo[articleName].comments.push({username, text});
+//     articlesInfo[articleName].upvotes +=1;
+//     res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
+// })
 
-    res.status(200).send(articlesInfo[articleName]);
-})
+// app.post("/api/articles/:name/add-comment", (req, res) => {
+//     const { username, text } = req.body;
+//     const articleName = req.params.name;
+
+//     articlesInfo[articleName].comments.push({username, text});
+
+//     res.status(200).send(articlesInfo[articleName]);
+// })
 
 app.listen(8000, ()=> console.log("listening on PORT 8000 "))
