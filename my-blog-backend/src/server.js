5f9cd80b41f887836f6844837a11547ee7a42c1a
Anand_Pawar <apawar3099@gmail.com>
2021-06-18 03:17:34 +0530

    added post method for getting upvotes and comments

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -5,15 +5,49 @@ const app = express();
 app.use(bodyParser.urlencoded())
 app.use(bodyParser.json())
 
-app.get("/hello", (req, res) =>{
-    res.send("Hello!!!")
-});
+// const articlesInfo ={
+//     'learn-react':{
+//         upvotes:0,
+//         comments : [],
+//     },
+//     'learn-node':{
+//         upvotes:0,
+//         comments : [],
+//     },
+//     'my-thoughts-on-resumes':{
+//         upvotes:0,
+//         comments : [],
+//     },
+// }  // Added in MongoDB , so NO use now
 
-app.get("/hello/:name", (req, res) => {
-    res.send(`Hello ${req.params.name}!`)
+// app.get("/hello", (req, res) =>{
+//     res.send("Hello!!!")
+// });
+
+// app.get("/hello/:name", (req, res) => {
+//     res.send(`Hello ${req.params.name}!`)
+// })
+// app.post("/hello", (req, res) =>{
+//     res.send(`Hello ${req.body.name}!`)
+// });
+
+
+app.
+
+app.post("/api/articles/:name/upvote", (req, res) => {
+    const articleName = req.params.name;
+
+    articlesInfo[articleName].upvotes +=1;
+    res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`)
 })
-app.post("/hello", (req, res) =>{
-    res.send(`Hello ${req.body.name}!`)
-});
+
+app.post("/api/articles/:name/add-comment", (req, res) => {
+    const { username, text } = req.body;
+    const articleName = req.params.name;
+
+    articlesInfo[articleName].comments.push({username, text});
+
+    res.status(200).send(articlesInfo[articleName]);
+})
 
 app.listen(8000, ()=> console.log("listening on PORT 8000 "))
